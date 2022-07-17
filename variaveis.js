2cbc428af86898c65fd2385700aace73a3193781
Erick Rodrigues <erick.prodrigues@ufpe.br>
2022-07-17 08:18:27 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,7 @@
+const myName = 'Erick';
+const birthCity = 'Caruaru';
+let birthYear = 1985;
+
+birthCity = 'Agrestina';
+
+console.log(birthCity); // O erro acontece ao tentar reatribuir um valor para uma variável constante
